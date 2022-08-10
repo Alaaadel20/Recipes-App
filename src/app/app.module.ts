17975fb0888451aafc0b5326206c55052bfd9248
Alaa Adel <alaaadelahmed50@gmail.com>
2022-08-11 00:12:25 +0200

    Edit App Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
@@ -16,7 +15,6 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent,
     RecipesListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
