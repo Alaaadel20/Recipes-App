3fba3089836af5534064210953970ce9e98c9d96
Alaa Adel <alaaadelahmed50@gmail.com>
2022-08-11 14:06:23 +0200

    Create & use recipe model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes:Recipe[]=[new Recipe('A test recipe','ma7shy','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/BBQ-halloumi-fajita-skewers-6bad040.jpg '),new Recipe('A test recipe','ma7shy','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/BBQ-halloumi-fajita-skewers-6bad040.jpg ')]
   constructor() { }
 
   ngOnInit(): void {
