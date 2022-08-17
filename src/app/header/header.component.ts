85e912bdadeb259503107046f60c3900493d0aed
Alaa Adel <alaaadelahmed50@gmail.com>
2022-08-17 16:48:40 +0200

    section 6 :Databinding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
 export class HeaderComponent {
+    //create event object and add output to pass it to app component:
+   @Output() featureSelected= new EventEmitter<string>();
+onSelect(feature: string){
+    //make the value of the event= feature which is the data injected for element in template ('recipe' & 'shopping-list')
+this.featureSelected.emit(feature)
 
+}
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
@@ -6,10 +6,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes:Recipe[]=[new Recipe('A test recipe','ma7shy','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/BBQ-halloumi-fajita-skewers-6bad040.jpg '),new Recipe('A test recipe','ma7shy','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/BBQ-halloumi-fajita-skewers-6bad040.jpg ')]
+ @Output() recipeWasSelected=new EventEmitter<Recipe>()
+  recipes:Recipe[]=[new Recipe('A test recipe','ma7shy','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/BBQ-halloumi-fajita-skewers-6bad040.jpg '),
+  new Recipe('Another test recipe','ma7shy','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/BBQ-halloumi-fajita-skewers-6bad040.jpg ')]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+onRecipeSelecetd(recipe:Recipe){
+  this.recipeWasSelected.emit(recipe)
+}
 }
